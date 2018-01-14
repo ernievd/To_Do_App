c789d6004c645ec99a69c65d8659ebca23442d5e
ernievd <ernievd@gmail.com>
2018-01-14 11:56:32 -0600

    Added the getTasks and can now display the all the basic information from the database to the DOM.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,9 +3,39 @@ $(document).ready(readySetGo);
 function readySetGo() {
     console.log('readySetGo working');
 
+    // Display tasks from database table
+    getTasks();
 
     // Event Listeners
+    $('#addTask').on('click', addTask);
+} // End readySetGo
 
+
+function getTasks() {
+    $.ajax({
+        method: 'GET',
+        url: '/toDo',
+        success: function(response) {
+            // empty the list on the DOM as to avoid appending
+            $('#table').empty();
+            console.log('response from getTask', response);
+            for (let i = 0; i < response.length; i++) {
+                const taskResponse = response[i];
+                let transferButton;
+
+                // Update the following to use template literals - back ticks
+                let $listItem = $(`<li class="taskItem jumbotron" data-id="${taskResponse.id}">`);
+                $listItem.append(`<h2 class="taskName" data-name="${taskResponse.task_name}">${taskResponse.task_name}</h2>`);
+                $listItem.append(`<p class="taskAssignee" data-assignee="${taskResponse.task_owner}">Owner: ${taskResponse.task_owner}</p>`);
+                $listItem.append(`<p class="gender" data-priorityr="${taskResponse.task_priority}">Priority: ${taskResponse.task_priority}</p>`);
+                $listItem.append(`<p class="notes">${taskResponse.notes}</p>`);
+
+                // $listItem.append(`<button class="deleteButton btn btn-info">Delete</button>`);
+                // $listItem.append(`<button class="editButton btn btn-info">Edit</button>`);
+                $('#taskList').prepend($listItem);
+            }
+        }
+    });
 }
 
 
