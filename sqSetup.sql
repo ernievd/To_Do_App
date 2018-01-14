77eacb13a66535a83738b1e28625ab28d24d70a5
ernievd <ernievd@gmail.com>
2018-01-14 13:09:27 -0600

    Updated to add task complete column

--- sqSetup.sql
+++ sqSetup.sql
@@ -3,12 +3,14 @@ CREATE TABLE tasks (
     task_name VARCHAR(50) NOT NULL,
     task_owner VARCHAR(50) NOT NULL,
     task_priority VARCHAR(10) NOT NULL,
+	task_complete BOOLEAN DEFAULT FALSE,
     notes VARCHAR(255) DEFAULT 'None'
 );
 
 
-INSERT INTO tasks (task_name, task_owner, task_priority, notes)
-VALUES ('Mow Lawn', 'Ernie', 'Medium', 'Bag the grass'), ('Wash Dishes', 'Ana', 'High', '');
+INSERT INTO tasks (task_name, task_owner, task_priority, task_complete, notes)
+VALUES ('Mow Lawn', 'Ernie', 'Medium', FALSE, 'Bag the grass'), ('Wash Dishes', 'Ana', 'High', FALSE, '');
 
-INSERT INTO tasks (task_name, task_owner, task_priority)
-VALUES ('Clean Garage', 'Corbin', 'Low');
+INSERT INTO tasks (task_name, task_owner, task_complete, task_priority)
+VALUES ('Clean Garage', 'Corbin', TRUE, 'Low');
+
