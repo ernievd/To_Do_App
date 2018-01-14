35d730226a7d5a1956352df2319cf0be72769ba2
ernievd <ernievd@gmail.com>
2018-01-14 11:15:18 -0600

    Added entries to include some default data in the table.

--- sqSetup.sql
+++ sqSetup.sql
@@ -2,6 +2,13 @@ CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     task_name VARCHAR(50) NOT NULL,
     task_owner VARCHAR(50) NOT NULL,
-    task_priority INT NOT NULL,
+    task_priority VARCHAR(10) NOT NULL,
     notes VARCHAR(255) DEFAULT 'None'
 );
+
+
+INSERT INTO tasks (task_name, task_owner, task_priority, notes)
+VALUES ('Mow Lawn', 'Ernie', 'Medium', 'Bag the grass'), ('Wash Dishes', 'Ana', 'High', '');
+
+INSERT INTO tasks (task_name, task_owner, task_priority)
+VALUES ('Clean Garage', 'Corbin', 'Low');
