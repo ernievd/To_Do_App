fd2de63bf85dea2afe5c1367d9cc0d3c96af8185
ernievd <ernievd@gmail.com>
2018-01-14 15:41:44 -0600

    Added put to update task complete

--- server/routes/toDo.router.js
+++ server/routes/toDo.router.js
@@ -18,4 +18,19 @@ router.get('/', function(req, res){
         });
 }); // End router.get
 
+router.put('/:id', function(req, res){
+    const queryText = 'UPDATE tasks SET task_complete=$1 WHERE id=$2;';
+    pool.query(queryText, [req.body.task_complete, req.params.id])
+    // runs on successful query
+        .then((result) => {
+            //console.log('query results: ', result);
+            res.send(200);
+        })
+        // error handling
+        .catch((err) => {
+            console.log('error making select query:', err);
+            res.sendStatus(500);
+        });
+}); // End router.put
+
 module.exports = router;
