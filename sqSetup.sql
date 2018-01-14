dc0c55ddf81d03140045d958765450787d15f02d
ernievd <ernievd@gmail.com>
2018-01-14 17:19:30 -0600

    Added more commands to create the join table and links

--- sqSetup.sql
+++ sqSetup.sql
@@ -14,3 +14,21 @@ VALUES ('Mow Lawn', 'Ernie', 'Medium', FALSE, 'Bag the grass'), ('Wash Dishes', 
 INSERT INTO tasks (task_name, task_owner, task_complete, task_priority)
 VALUES ('Clean Garage', 'Corbin', TRUE, 'Low');
 
+
+CREATE TABLE catagories (
+    id SERIAL PRIMARY KEY,
+    catagory_name VARCHAR(25) NOT NULL
+);
+
+INSERT INTO catagories (catagory_name)
+VALUES ('Work Task'), ('Home Task'), ('School Task');
+
+
+CREATE TABLE tasks_catagories (
+    id SERIAL PRIMARY KEY,
+    tasks_id INT REFERENCES tasks,
+    catagories_id INT REFERENCES catagories
+);
+
+INSERT INTO tasks_catagories (tasks_id, catagories_id)
+VALUES (1,2), (2,2);
